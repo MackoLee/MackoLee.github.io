d3f2434afaef93f4c41eab4a89cd1145eedc05ac
MackoLee <sangok1993@naver.com>
2025-09-11 19:15:42 +0000

    Deploying to gh-pages from @ MackoLee/MackoLee.github.io@aa3f9460634170adeb2e4083c5c997a6c17bc3fb 🚀

---
+++ Brlba50K.js
@@ -0,0 +1,79 @@
+const t={name:{t:0,b:{t:2,i:[{t:3}],s:"Yoon-Gun Lee"}},job:{t:0,b:{t:2,i:[{t:3}],s:"AI & Automation Frontend Developer"}},language:{t:0,b:{t:2,i:[{t:3}],s:"English"}},visit:{t:0,b:{t:2,i:[{t:3}],s:"Visit"}},rights:{t:0,b:{t:2,i:[{t:3}],s:"All rights reserved."}},phone:{t:0,b:{t:2,i:[{t:3}],s:"+82-10-2527-2927"}},email:{t:0,b:{t:2,i:[{t:3}],s:"sangok1993/at/naver.com"}},about:{t:0,b:{t:2,i:[{t:3}],s:"About"}},about_content:{title:{t:0,b:{t:2,i:[{t:3}],s:"Yoon-Gun Lee, a developer who learns and grows with value."}},first:{t:0,b:{t:2,i:[{t:3}],s:"First"}},first_content:{t:0,b:{t:2,i:[{t:3}],s:"Led app and web app development projects, successfully managing the entire process from design to implementation and automation. Significantly improved development productivity by introducing a deployment process through separating Staging and Production servers and automating version control using Git Tags."}},second:{t:0,b:{t:2,i:[{t:3}],s:"Second"}},second_content:{t:0,b:{t:2,i:[{t:3}],s:"Continuously growing by learning new technologies and applying them to real projects. Introduced Zustand, Tailwindcss, SSR, GraphQL, designed Backend Architecture, and conducted personal projects through Google Console, applying insights gained to company projects."}},third:{t:0,b:{t:2,i:[{t:3}],s:"Third"}},third_content:{t:0,b:{t:2,i:[{t:3}],s:"Beyond mastering technology, I strive to be a developer who understands business goals and contributes my skills. I listened directly to the difficulties faced by an acquaintance running a music academy in managing students, teachers, and the academy, and started an app development project to solve them."}},fourth:{t:0,b:{t:2,i:[{t:3}],s:"I will continue to be a developer who adds practical value using my skills."}}},experience:{t:0,b:{t:2,i:[{t:3}],s:"Experience"}},experience_items:{total_experience:{t:0,b:{t:2,i:[{t:3}],s:"4 years 3 months experience"}},super_run:{company:{t:0,b:{t:2,i:[{t:3}],s:"SuperRun"}},date:{t:0,b:{t:2,i:[{t:3}],s:"2025.08 - Present (2 months) Full-time"}},badge:{t:0,b:{t:2,i:[{t:3}],s:"Currently Employed"}},projects:{project1:{heading:{t:0,b:{t:2,i:[{t:3}],s:"Research in progress for Startup Growth Technology Development Project"}},date:{t:0,b:{t:2,i:[{t:3}],s:"2025.09 - 2025.09"}},description:{t:0,b:{t:2,i:[{t:3}],s:"Startup Growth Technology Development Project (Stepping Stone) - Research Server Setup"}},members:{t:0,b:{t:2,i:[{t:3}],s:"Personnel: 1 Developer"}},detail:{t:0,b:{t:2,i:[{t:3}],s:`
+1. Analysis Server Setup
+	- Designed and built a server for achieving research results: server design utilizing fastapi + celery + redis (100%)`}}}}},repich:{company:{t:0,b:{t:2,i:[{t:3}],s:"Repich Co., Ltd."}},date:{t:0,b:{t:2,i:[{t:3}],s:"2024.06 - 2025.08 (1 year 3 months) Full-time"}},projects:{project1:{heading:{t:0,b:{t:2,i:[{t:3}],s:"LG CNS On-Device AI Challenge Performance Presentation"}},date:{t:0,b:{t:2,i:[{t:3}],s:"2024.06 - 2024.09"}},description:{t:0,b:{t:2,i:[{t:3}],s:"Achievement: Selected among 10 out of 128 companies and participated in booth operation"}},members:{t:0,b:{t:2,i:[{t:3}],s:"Personnel: 1 Developer, 1 Designer"}},detail:{t:0,b:{t:2,i:[{t:3}],s:`
+1. Smart Player Development (90%)
+	- Smart player with STT subtitles, AI video segment generation, AI summary, AI Q&A, and note-taking for learners
+2. Planning: Collaboration with designer (30%)
+	- Electron + Nextjs (SPA) exe application development (100%)
+3. Backend and Video Analysis Server Development (100%)
+	- STT service, AI summary, AI Q&A function development (100%)
+	- Database design (100%)
+	- Backend development using Nestjs + FFmpeg + GoogleSTT (100%)
+4. Infrastructure Setup (100%)
+	- Server and DB setup using Kakao Cloud VM (100%)`}}},project2:{heading:{t:0,b:{t:2,i:[{t:3}],s:"Administrator Page Development and Player Advancement"}},date:{t:0,b:{t:2,i:[{t:3}],s:"2024.10 - 2024.12"}},description:{t:0,b:{t:2,i:[{t:3}],s:"Achievement: Added player video upload and STT management functions."}},members:{t:0,b:{t:2,i:[{t:3}],s:"Personnel: 2 Developers, 1 Designer"}},detail:{t:0,b:{t:2,i:[{t:3}],s:`
+1. Smart Player Development (90%)
+	- Refined the Nextjs-based player into a SaaS service after not being selected by 3 LG CNS companies
+	- Technical debt and bug fixes due to LG CNS MVP application development (100%)
+	- Markdown note-taking function introduced (100%)
+2. Planning: Collaboration with designer (30%)
+3. Administrator Page Development (50%)
+	- Site development for effective video management.
+	- Video management system development (50%)
+4. Backend (100%)
+	- GraphQL introduction (100%)
+5. Infrastructure Setup (80%)
+	- Video storage and retrieval system built using Google Cloud VM, Database, and Storage (100%)`}}},project3:{heading:{t:0,b:{t:2,i:[{t:3}],s:"LMS Development"}},date:{t:0,b:{t:2,i:[{t:3}],s:"2024.11 - 2025.08"}},description:{t:0,b:{t:2,i:[{t:3}],s:"Achievement: Introduced course packaging and payment system. Separated Production, Staging servers and Database. Automated deployment."}},members:{t:0,b:{t:2,i:[{t:3}],s:"Personnel: 3 Developers, 2 Designers"}},detail:{t:0,b:{t:2,i:[{t:3}],s:`
+1. Smart Player Development (90%)
+	- Added functionality to view packaged courses, track progress, and execute them (100%).
+
+2. Administrator Page (30%)
+	- Page for registering and packaging courses, and managing students and staff.
+3. Video Management Function Development
+	- Platform Page (20%)
+	- Main page where users can purchase academy courses or view instructor profiles.
+	- Designed and developed to show different screen compositions based on domain (80%).
+4. Backend (70%)
+	- Developed APIs required for administrator and platform pages (60%).
+	- Introduced payment (KG Inicis) and deployment (git action) automation, and separated Production server and Database (80%).
+5. Infrastructure (80%)
+	- Applied Nginx wildcard domain and SSL, and separated servers based on URL.`}}},project4:{heading:{t:0,b:{t:2,i:[{t:3}],s:"SME Technology Development (R&D) Support Project Research Test in Progress"}},date:{t:0,b:{t:2,i:[{t:3}],s:"2025.07 - 2025.09"}},detail:{t:0,b:{t:2,i:[{t:3}],s:`
+1. Preparation of Public Testing Agency Test Request Standards (20%)
+2. Test environment configuration and execution (100%).`}}}}},eipgrid:{company:{t:0,b:{t:2,i:[{t:3}],s:"EIPGRID Co., Ltd."}},date:{t:0,b:{t:2,i:[{t:3}],s:"2022.08 - 2024.06 (1 year 11 months) Full-time"}},projects:{project1:{heading:{t:0,b:{t:2,i:[{t:3}],s:"Research and Development of Analytical Application"}},date:{t:0,b:{t:2,i:[{t:3}],s:"2022.06 - 2024.04"}},link:{t:0,b:{t:2,i:[{t:3}],s:"https://www.eipgrid.com/dare"}},detail:{t:0,b:{t:2,i:[{t:3}],s:`
+1. Data Analysis and Comparison Tool: A tool for comparing various algorithms (D1)
+	- ESS charge/discharge management algorithm (code refactoring and bug fixes)
+	- Designed and developed with consultation from the company's CTO
+2. Web Application Production (90%)
+	- Migrated from Vue2, Webpack to Vue3, Vite
+	- Backend (Flask) Rest API (40%)
+	- Dev and Prd deployment and environment management using Docker, Web bundler (50%)
+3. Data Visualization Tool: A tool for creating, listing, saving, and sharing charts externally (D3)
+	- Designed and developed by a team of 2 developers with consultation from the company's CTO
+	- Web application production (50%)
+	- Backend (Flask) Rest API and service logic design and development (40%)
+	- Dev and Prd deployment and environment management using Docker, Web bundler (30%)
+	- PostgreSQL procedure functions and DB creation (60%)`}}},project2:{heading:{t:0,b:{t:2,i:[{t:3}],s:"Electric Vehicle Charging Management Application Development (Demo)"}},date:{t:0,b:{t:2,i:[{t:3}],s:"2023.11 - 2024.04"}},description:{t:0,b:{t:2,i:[{t:3}],s:"An application that receives and manages real-time information about electric vehicle charging"}},detail:{t:0,b:{t:2,i:[{t:3}],s:`
+1. Backend (Java) Rest API Design and Development (30%)
+2. Web Application (Vue) Development (40%)
+	- Introduced a new UI Framework and state management tool with colleagues.
+3. Analysis Server Development (20%)
+	- Truck dispatch algorithm (50%)
+	- Algorithm for selecting trucks by calculating distances between vehicles (code refactoring and bug fixes)
+	- Improved GPS distance calculation logic speed. 14.5 seconds -> 0.5 seconds (O(n * n * n) -> O(n * n))
+5. Deployed analysis server to AWS using Docker
+6. Successfully completed design and feature changes the day before the showcase by working through the night.`}}},project3:{heading:{t:0,b:{t:2,i:[{t:3}],s:"Retail VPP Application Development (Demo)"}},date:{t:0,b:{t:2,i:[{t:3}],s:"2024.01 - 2024.04"}},description:{t:0,b:{t:2,i:[{t:3}],s:"A platform application where retailers can save electricity and earn money in the market"}},detail:{t:0,b:{t:2,i:[{t:3}],s:`
+1. Analysis Server Algorithm Development
+	- Developed scheduling logic using Redis in preparation for distributed environments (30%)
+2. Backend (FastAPI) Rest API Development
+	- Migrated Flask application to FastAPI
+3. Web Application (Vue) Development
+	- Introduced Tailwindcss
+4. Deployed analysis server to AWS using Docker`}}}}},ion_communications:{company:{t:0,b:{t:2,i:[{t:3}],s:"ION Communications Co., Ltd."}},date:{t:0,b:{t:2,i:[{t:3}],s:"2021.12 - 2022.08 (9 months)"}},projects:{project1:{heading:{t:0,b:{t:2,i:[{t:3}],s:"EIPGRID Headquarters Spinoff"}},date:{t:0,b:{t:2,i:[{t:3}],s:"2022.08 - 2022.08"}},detail:{t:0,b:{t:2,i:[{t:3}],s:"- EIPGRID Headquarters Spinoff"}}}}},majoonsoft:{company:{t:0,b:{t:2,i:[{t:3}],s:"Majoonsoft Co., Ltd."}},date:{t:0,b:{t:2,i:[{t:3}],s:"2020.11 - 2021.03 (5 months)"}},projects:{project1:{heading:{t:0,b:{t:2,i:[{t:3}],s:"Management Site Security Feature Development (PHP)"}},date:{t:0,b:{t:2,i:[{t:3}],s:"2020.12 - 2021.01"}},description:{t:0,b:{t:2,i:[{t:3}],s:"Due to managing login information only with LocalStorage and JWT for login convenience, the administrator cookie was stolen, and a hacker logged in as an administrator and deleted all important information."}},detail:{t:0,b:{t:2,i:[{t:3}],s:`
+	- Added a function that requires password entry to change important information.`}}},project2:{heading:{t:0,b:{t:2,i:[{t:3}],s:"Traffic Management IDC"}},date:{t:0,b:{t:2,i:[{t:3}],s:"2020.12 - 2021.03"}},description:{t:0,b:{t:2,i:[{t:3}],s:"Real-time traffic graph confirmation and QoS setting and port blocking tasks by accessing switches via ssh."}},members:{t:0,b:{t:2,i:[{t:3}],s:"Personnel: 1 Developer"}},detail:{t:0,b:{t:2,i:[{t:3}],s:`
+	- Experienced solving DDoS attacks on servers in collaboration with KT using Linux.`}}}}}},skills:{t:0,b:{t:2,i:[{t:3}],s:"Skills"}},skill_categories:{languages:{name:{t:0,b:{t:2,i:[{t:3}],s:"Languages"}},skills:[{t:0,b:{t:2,i:[{t:3}],s:"Python"}},{t:0,b:{t:2,i:[{t:3}],s:"JavaScript"}},{t:0,b:{t:2,i:[{t:3}],s:"Java"}},{t:0,b:{t:2,i:[{t:3}],s:"TypeScript"}},{t:0,b:{t:2,i:[{t:3}],s:"C++"}}]},frameworks:{name:{t:0,b:{t:2,i:[{t:3}],s:"Frameworks & Libraries"}},skills:[{t:0,b:{t:2,i:[{t:3}],s:"VueJS"}},{t:0,b:{t:2,i:[{t:3}],s:"Git"}},{t:0,b:{t:2,i:[{t:3}],s:"Spring Framework"}},{t:0,b:{t:2,i:[{t:3}],s:"React.js"}},{t:0,b:{t:2,i:[{t:3}],s:"Flask"}}]},databases:{name:{t:0,b:{t:2,i:[{t:3}],s:"Databases"}},skills:[{t:0,b:{t:2,i:[{t:3}],s:"PostgreSQL"}},{t:0,b:{t:2,i:[{t:3}],s:"MySQL"}}]},devops:{name:{t:0,b:{t:2,i:[{t:3}],s:"DevOps & Cloud"}},skills:[{t:0,b:{t:2,i:[{t:3}],s:"EC2"}},{t:0,b:{t:2,i:[{t:3}],s:"Ubuntu"}},{t:0,b:{t:2,i:[{t:3}],s:"Docker"}}]}},education:{t:0,b:{t:2,i:[{t:3}],s:"Education"}},education_items:{ajou_university:{school:{t:0,b:{t:2,i:[{t:3}],s:"Ajou University Graduate School of Information and Communication"}},date:{t:0,b:{t:2,i:[{t:3}],s:"2018.03 - 2020.02 Information and Communication Engineering"}},description:{t:0,b:{t:2,i:[{t:3}],s:"Completed Intelligent Software. Expected to graduate after writing a thesis if a research project is found after completion."}}},korea_aerospace_university:{school:{t:0,b:{t:2,i:[{t:3}],s:"Korea Aerospace University"}},date:{t:0,b:{t:2,i:[{t:3}],s:"2012.03 - 2016.02 Software Engineering"}},description:{t:0,b:{t:2,i:[{t:3}],s:"Research: Bicycle Navigation. Achievement: Top graduate in 2016. Guidance using communication between Android application and laser. Java (Android Studio) - API 연동 및 로직 개발(50%), UI 개발(50%). Arduino - Directional laser output logic development."}}}},portfolio:{t:0,b:{t:2,i:[{t:3}],s:"Portfolio"}},portfolio_items:[{title:{t:0,b:{t:2,i:[{t:3}],s:"Pickclass"}},subtitle:{t:0,b:{t:2,i:[{t:3}],s:`Platform connecting students, teachers, and directors 
+ 1-person development 
+ test/at/test.com / test123`}},detail:{t:0,b:{static:"",t:2,i:[]}},href:{t:0,b:{t:2,i:[{t:3}],s:"https://픽클.com"}}},{title:{t:0,b:{t:2,i:[{t:3}],s:"Travel Duo"}},subtitle:{t:0,b:{t:2,i:[{t:3}],s:`Travel record 
+ 2-person development 
+ iOS, Android Flutter App + Web View`}},detail:{t:0,b:{static:"",t:2,i:[]}},href:{t:0,b:{t:2,i:[{t:3}],s:"https://apps.apple.com/kr/app/%ED%8A%B8%EB%9E%98%EB%B8%94%EB%93%80%EC%98%A4/id6670733527"}}},{title:{t:0,b:{t:2,i:[{t:3}],s:"STAMP Note"}},subtitle:{t:0,b:{t:2,i:[{t:3}],s:`Coupon system product planning and frontend development 
+ nuxtjs`}},detail:{t:0,b:{static:"",t:2,i:[]}},href:{t:0,b:{t:2,i:[{t:3}],s:"https://mackolee.github.io/stamp-note/"}}},{title:{t:0,b:{t:2,i:[{t:3}],s:"Todo Calendar"}},subtitle:{t:0,b:{t:2,i:[{t:3}],s:`Web application development to add tasks to calendar 
+ nextjs`}},detail:{t:0,b:{static:"",t:2,i:[]}},href:{t:0,b:{t:2,i:[{t:3}],s:"https://mackolee.github.io/todo-calendar/"}}},{title:{t:0,b:{t:2,i:[{t:3}],s:"Coding Test UI"}},subtitle:{t:0,b:{t:2,i:[{t:3}],s:`Coding test UI development 
+ nuxtjs`}},detail:{t:0,b:{static:"",t:2,i:[]}},href:{t:0,b:{t:2,i:[{t:3}],s:"https://mackolee.github.io/coding/"}}},{title:{t:0,b:{t:2,i:[{t:3}],s:"Play Game"}},subtitle:{t:0,b:{static:"",t:2,i:[]}},detail:{t:0,b:{static:"",t:2,i:[]}},href:{t:0,b:{t:2,i:[{t:3}],s:"https://mackolee.github.io/play-game/"}}}]};export{t as default};
